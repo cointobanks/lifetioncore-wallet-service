c64a50535c18b61de19066baaf0f23e33dcf8f2d
snogcel <ledniknoj@gmail.com>
2016-07-03 15:43:45 -0600

    blockchain sync, SPV transaction verification

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var BWS = {};
 BWS.Server = require('./lib/server');
 BWS.Storage = require('./lib/storage');
 BWS.MessageBroker = require('./lib/messagebroker');
+BWS.Blockchain = require('./lib/blockchain');
 BWS.BlockchainExplorer = require('./lib/blockchainexplorer');
 BWS.FiatRateService = require('./lib/fiatrateservice');
 BWS.Common = require('./lib/common');
