044e9a34c173a7b3faa0fa6dabdd01a58c3d9815
Alex Werner <alex@werner.fr>
2017-04-22 00:38:10 +0200

    Updated: blockchain explorer to the right uri.

--- config.js
+++ config.js
@@ -38,11 +38,11 @@ var config = {
   blockchainExplorerOpts: {
     livenet: {
       provider: 'insight',
-      url: 'https://insight.bitpay.com:443',
+      url: 'https://insight.dash.org:443/insight',
     },
     testnet: {
       provider: 'insight',
-      url: 'https://test-insight.bitpay.com:443',
+      url: 'https://dev-test.dash.org:443',
       // url: 'http://localhost:3001',
       // Multiple servers (in priority order)
       // url: ['http://a.b.c', 'https://test-insight.bitpay.com:443'],
